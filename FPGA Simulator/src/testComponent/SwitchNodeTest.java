3cda3f4d7b656e09c65e7b6cd95660a735962740
Wriliz <sys.dany+github@gmail.com>
2016-11-29 16:17:24 +0100

    Ajout SwitchNodeTest fonctionnel

---
+++ FPGA Simulator/src/testComponent/SwitchNodeTest.java
@@ -0,0 +1,34 @@
+package testComponent;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import component.SwitchNode;
+
+public class SwitchNodeTest {
+
+	SwitchNode swn;
+	@Before
+	public void setUp() throws Exception {
+		swn = new SwitchNode();
+	}
+
+	@Test
+	public void init() {
+		assertNotNull("L'initialisation à échoué",swn);
+	}
+	
+	@Test
+	public void test() {
+		swn.setInputs("1");
+		assertEquals("10", swn.getOutputs() );
+		swn.switchEtat();
+		assertEquals("11", swn.getOutputs() );
+		
+		swn.setInputs("0");
+		assertEquals("00", swn.getOutputs() );
+		swn.switchEtat();
+		assertEquals("00", swn.getOutputs() );
+		
+	}
+
+}
