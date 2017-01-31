d2ed16331e1d6a8743e18f9bcb1306e0e597b872
Dany Sysavath <Dany Sysavath@SsdPortableDany>
2017-01-31 16:24:21 +0100

    Ajout TestWindow

--- FPGA Simulator/src/Interface/TestWindow.java
+++ FPGA Simulator/src/Interface/TestWindow.java
@@ -72,6 +72,7 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JFormattedTextField;
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
+import java.awt.Label;
 
 public class TestWindow {
 
@@ -85,6 +86,16 @@ public class TestWindow {
 	private final ButtonGroup Answers = new ButtonGroup();
 	private JTextField textField;
 	private String typeReponse;
+	private JTextField AnswNbBitsLUT;
+	private JTextField AnswNbLUT;
+	private JTextField AnswNbInputs;
+	private int NbLut = 4;
+	private int NbBitLut = 3;
+	private int NbIn = 6;
+	private int NbPossibleAns = 3;
+	private JTextField NbQCMAns;
+	private boolean PossibleMultipleAnsw;
+
 	/**
 	 * Launch the application.
 	 */
@@ -117,74 +128,162 @@ public class TestWindow {
 		frmTest.setBounds(0, 0, 3840, 2160);
 		frmTest.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTest.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel LeftPanel = new JPanel();
 		frmTest.getContentPane().add(LeftPanel, BorderLayout.WEST);
 		LeftPanel.setLayout(new BoxLayout(LeftPanel, BoxLayout.PAGE_AXIS));
-		
+
 		JCheckBox chckbxAfficherSimulateur = new JCheckBox("Afficher simulateur");
 		chckbxAfficherSimulateur.setSelected(true);
 		LeftPanel.add(chckbxAfficherSimulateur);
-		
+
 		JTree tree = new JTree();
+		tree.addTreeSelectionListener(new TreeSelectionListener() {
+			public void valueChanged(TreeSelectionEvent arg0) {
+				JOptionPane.showMessageDialog(null, tree.getLastSelectedPathComponent());
+			}
+		});
 		tree.setAlignmentY(Component.TOP_ALIGNMENT);
 		tree.setAlignmentX(Component.LEFT_ALIGNMENT);
-		tree.setModel(new DefaultTreeModel(
-			new DefaultMutableTreeNode("Table des mati\u00E8res") {
-				{
-					DefaultMutableTreeNode node_1;
-					node_1 = new DefaultMutableTreeNode("Exercice 1");
-						node_1.add(new DefaultMutableTreeNode("Question 1"));
-						node_1.add(new DefaultMutableTreeNode("Question 2"));
-						node_1.add(new DefaultMutableTreeNode("+Ajouter une question"));
-					add(node_1);
-					node_1 = new DefaultMutableTreeNode("Exercice 2");
-						node_1.add(new DefaultMutableTreeNode("Question 1"));
-						node_1.add(new DefaultMutableTreeNode("Question 2"));
-						node_1.add(new DefaultMutableTreeNode("+Ajouter une question"));
-					add(node_1);
-					add(new DefaultMutableTreeNode("+Ajouter un exercice"));
-				}
+		tree.setModel(new DefaultTreeModel(new DefaultMutableTreeNode("Table des mati\u00E8res") {
+			{
+				DefaultMutableTreeNode node_1;
+				node_1 = new DefaultMutableTreeNode("Exercice 1");
+				node_1.add(new DefaultMutableTreeNode("Question 1"));
+				node_1.add(new DefaultMutableTreeNode("Question 2"));
+				node_1.add(new DefaultMutableTreeNode("+Ajouter une question"));
+				add(node_1);
+				node_1 = new DefaultMutableTreeNode("Exercice 2");
+				node_1.add(new DefaultMutableTreeNode("Question 1"));
+				node_1.add(new DefaultMutableTreeNode("Question 2"));
+				node_1.add(new DefaultMutableTreeNode("+Ajouter une question"));
+				add(node_1);
+				add(new DefaultMutableTreeNode("+Ajouter un exercice"));
 			}
-		));
+		}));
 		LeftPanel.add(tree);
-		//JScrollPane scrollPane = new JScrollPane(tree);
-		//leftPanel.add(scrollPane);
-		
-		/*ImageIcon imageIcon = new ImageIcon();
-		DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();
-		renderer.setLeafIcon(imageIcon);*/
-		
+
+		JPanel SimulatorSettingsPanel = new JPanel();
+		frmTest.getContentPane().add(SimulatorSettingsPanel, BorderLayout.CENTER);
+		SimulatorSettingsPanel.setVisible(false);
+		SimulatorSettingsPanel.setLayout(new GridLayout(0, 1, 0, 0));
+
+		JPanel NbBitsLUTPanel = new JPanel();
+		SimulatorSettingsPanel.add(NbBitsLUTPanel);
+		NbBitsLUTPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		Label NbBitsLUT = new Label("Nombre de bits du LUT:");
+		NbBitsLUTPanel.add(NbBitsLUT);
+
+		AnswNbBitsLUT = new JTextField();
+		NbBitsLUTPanel.add(AnswNbBitsLUT);
+		AnswNbBitsLUT.setText(String.valueOf(NbBitLut));
+		AnswNbBitsLUT.setColumns(10);
+
+		JPanel NbLUTPanel = new JPanel();
+		SimulatorSettingsPanel.add(NbLUTPanel);
+
+		Label NbLUT = new Label("Nombre de LUTs:");
+		NbLUTPanel.add(NbLUT);
+
+		AnswNbLUT = new JTextField();
+		NbLUTPanel.add(AnswNbLUT);
+		AnswNbLUT.setText(String.valueOf(NbLut));
+		AnswNbLUT.setColumns(10);
+
+		JPanel NbInputsPanel = new JPanel();
+		SimulatorSettingsPanel.add(NbInputsPanel);
+		NbInputsPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		Label NbInputs = new Label("Nombre d'entr\u00E9es:");
+		NbInputsPanel.add(NbInputs);
+
+		AnswNbInputs = new JTextField();
+		NbInputsPanel.add(AnswNbInputs);
+		AnswNbInputs.setText(String.valueOf(NbIn));
+		AnswNbInputs.setColumns(10);
+
+		JTabbedPane AnsParamTab = new JTabbedPane(JTabbedPane.TOP);
+		AnsParamTab.setVisible(false);
+		frmTest.getContentPane().add(AnsParamTab, BorderLayout.CENTER);
+
+		JPanel QCM = new JPanel();
+		QCM.setVisible(false);
+		AnsParamTab.addTab("New tab", null, QCM, null);
+		QCM.setLayout(new GridLayout(0, 1, 0, 0));
+
+		JCheckBox MultipleAnsw = new JCheckBox("R\u00E9ponses multiples");
+		MultipleAnsw.setSelected(true);
+		PossibleMultipleAnsw = MultipleAnsw.isSelected();
+		QCM.add(MultipleAnsw);
+
+		JPanel NbAnsPan = new JPanel();
+		QCM.add(NbAnsPan);
+
+		JLabel NbAns = new JLabel("Nombre de r\u00E9ponses");
+		NbAnsPan.add(NbAns);
+
+		NbQCMAns = new JTextField();
+		NbAnsPan.add(NbQCMAns);
+		NbQCMAns.setText(String.valueOf(NbPossibleAns));
+		NbQCMAns.setColumns(10);
+
+		JPanel Others = new JPanel();
+		Others.setVisible(false);
+		AnsParamTab.addTab("New tab", null, Others, null);
+
+		JLabel lblTab_1 = new JLabel("Pas de r\u00E9glages possibles");
+		Others.add(lblTab_1);
+
 		JPanel CenterPanel = new JPanel();
 		frmTest.getContentPane().add(CenterPanel, BorderLayout.CENTER);
 		CenterPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel SouthPanel = new JPanel();
 		CenterPanel.add(SouthPanel, BorderLayout.SOUTH);
 		SouthPanel.setLayout(new GridLayout(1, 0, 0, 0));
-		
 		JPanel SimulatorAnswerPanel = new JPanel();
 		CenterPanel.add(SimulatorAnswerPanel, BorderLayout.CENTER);
 		SimulatorAnswerPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel AnswerPanel = new JPanel();
 		SimulatorAnswerPanel.add(AnswerPanel, BorderLayout.SOUTH);
 		AnswerPanel.setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		JPanel panel = new JPanel();
 		FlowLayout flowLayout_3 = (FlowLayout) panel.getLayout();
 		flowLayout_3.setAlignment(FlowLayout.LEFT);
 		AnswerPanel.add(panel);
-		
+
 		JButton btnModifierLeSimulateur = new JButton("Modifier le simulateur");
 		btnModifierLeSimulateur.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Ouverture des param\u00E8tres du simulateur");
+				SimulatorSettingsPanel.setVisible(true);
+				int result = JOptionPane.showConfirmDialog(null, SimulatorSettingsPanel, "",
+						JOptionPane.OK_CANCEL_OPTION);
+				if (result == JOptionPane.OK_OPTION) {
+					/*
+					 * System.out.println("Nombre de bits pour le LUT: " +
+					 * AnswNbBitsLUT.getText()); //si besoin affichage dans la
+					 * console System.out.println("Nombre de LUTs: " +
+					 * AnswNbLUT.getText());
+					 * System.out.println("Nombre d'entrées: " +
+					 * AnswNbInputs.getText());
+					 */
+					NbBitLut = Integer.parseInt(AnswNbBitsLUT.getText());
+					NbIn = Integer.parseInt(AnswNbInputs.getText());
+					NbLut = Integer.parseInt(AnswNbLUT.getText());
+				} else {
+					AnswNbInputs.setText(String.valueOf(NbIn));
+					AnswNbLUT.setText(String.valueOf(NbLut));
+					AnswNbBitsLUT.setText(String.valueOf(NbBitLut));
+				}
+				SimulatorSettingsPanel.setVisible(false);
 			}
 		});
 		panel.add(btnModifierLeSimulateur);
-		
+
 		JButton btnRinitialiserSimulateur = new JButton("R\u00E9initialiser ce simulateur");
 		panel.add(btnRinitialiserSimulateur);
 		btnRinitialiserSimulateur.setMinimumSize(new Dimension(100, 23));
@@ -195,74 +294,98 @@ public class TestWindow {
 				JOptionPane.showMessageDialog(null, "R\u00E9initialisation du simulateur");
 			}
 		});
-		
+
 		JPanel AnswerTypePanel = new JPanel();
 		FlowLayout flowLayout_2 = (FlowLayout) AnswerTypePanel.getLayout();
 		flowLayout_2.setAlignment(FlowLayout.LEFT);
 		AnswerPanel.add(AnswerTypePanel);
-		
+
 		JComboBox comboBox = new JComboBox();
 		comboBox.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent arg0) {
-				typeReponse=arg0.getItem().toString();
+				typeReponse = arg0.getItem().toString();
 				JOptionPane.showMessageDialog(null, typeReponse);
 			}
 		});
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"QCM", "Zone de texte", "R\u00E9ponse sur simulateur", "Checkbox"}));
+		comboBox.setModel(
+				new DefaultComboBoxModel(new String[] { "QCM", "Zone de texte", "R\u00E9ponse sur simulateur" }));
 		comboBox.setToolTipText("");
 		AnswerTypePanel.add(comboBox);
-		
+
 		JButton btnParamtres = new JButton("Param\u00E8tres");
+
 		btnParamtres.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Param\u00E8tres de la zone");
+				// JOptionPane.showMessageDialog(null, "Param\u00E8tres de la
+				// zone");
+				if (comboBox.getSelectedItem() == "QCM") {
+					QCM.setVisible(true);
+					int result = JOptionPane.showConfirmDialog(null, QCM, "", JOptionPane.OK_CANCEL_OPTION);
+					if (result == JOptionPane.OK_OPTION) {
+						PossibleMultipleAnsw = MultipleAnsw.isSelected();
+						NbPossibleAns = Integer.parseInt(NbQCMAns.getText());
+					} else {
+						NbQCMAns.setText(String.valueOf(NbPossibleAns));
+						if (PossibleMultipleAnsw) {
+							MultipleAnsw.setSelected(true);
+						} else {
+							MultipleAnsw.setSelected(false);
+						}
+					}
+					QCM.setVisible(false);
+				} else {
+					Others.setVisible(true);
+					int result = JOptionPane.showConfirmDialog(null, Others, "", JOptionPane.CLOSED_OPTION);
+					Others.setVisible(false);
+				}
 			}
 		});
 		AnswerTypePanel.add(btnParamtres);
-		
+
 		JPanel ReponseA = new JPanel();
 		FlowLayout fl_ReponseA = (FlowLayout) ReponseA.getLayout();
 		fl_ReponseA.setAlignment(FlowLayout.LEFT);
 		AnswerPanel.add(ReponseA);
-		
+
 		JRadioButton rdbtnA = new JRadioButton("A");
 		ReponseA.add(rdbtnA);
 		Answers.add(rdbtnA);
-		
+
 		JTextPane txtpnRponse = new JTextPane();
 		ReponseA.add(txtpnRponse);
 		txtpnRponse.setText("R\u00E9ponse 1");
-		
+
 		JPanel ReponseB = new JPanel();
 		FlowLayout fl_ReponseB = (FlowLayout) ReponseB.getLayout();
 		fl_ReponseB.setAlignment(FlowLayout.LEFT);
 		AnswerPanel.add(ReponseB);
-		
+
 		JRadioButton rdbtnB = new JRadioButton("B");
 		ReponseB.add(rdbtnB);
 		Answers.add(rdbtnB);
-		
+
 		JTextPane txtpnRponse_1 = new JTextPane();
 		ReponseB.add(txtpnRponse_1);
 		txtpnRponse_1.setText("R\u00E9ponse 2");
-		
+
 		JPanel ReponseC = new JPanel();
 		FlowLayout fl_ReponseC = (FlowLayout) ReponseC.getLayout();
 		fl_ReponseC.setAlignment(FlowLayout.LEFT);
 		AnswerPanel.add(ReponseC);
-		
+
 		JRadioButton rdbtnC = new JRadioButton("C");
 		ReponseC.add(rdbtnC);
 		Answers.add(rdbtnC);
-		
+
 		JTextPane txtpnRponse_2 = new JTextPane();
 		ReponseC.add(txtpnRponse_2);
 		txtpnRponse_2.setText("R\u00E9ponse 3");
-		
+
 		JTabbedPane SimulatorChoice = new JTabbedPane(JTabbedPane.TOP);
+
 		SimulatorAnswerPanel.add(SimulatorChoice, BorderLayout.CENTER);
-		
+
 		txtL = new JTextField();
 		SimulatorChoice.addTab("Etat initial", null, txtL, null);
 		txtL.setHorizontalAlignment(SwingConstants.CENTER);
@@ -271,7 +394,7 @@ public class TestWindow {
 		txtL.setSelectedTextColor(Color.CYAN);
 		txtL.setText("Simulateur \u00E0 ins\u00E9rer ici");
 		txtL.setColumns(10);
-		
+
 		textField = new JTextField();
 		SimulatorChoice.addTab("R\u00E9sultat attendu", null, textField, null);
 		textField.setText("Simulateur \u00E0 ins\u00E9rer ici");
@@ -280,47 +403,47 @@ public class TestWindow {
 		textField.setForeground(Color.BLACK);
 		textField.setColumns(10);
 		textField.setBackground(Color.GREEN);
-		
+
 		JPanel EnoncePanel = new JPanel();
 		CenterPanel.add(EnoncePanel, BorderLayout.NORTH);
 		EnoncePanel.setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		JPanel ExerciceNbTitlePanel = new JPanel();
 		FlowLayout flowLayout_1 = (FlowLayout) ExerciceNbTitlePanel.getLayout();
 		flowLayout_1.setAlignment(FlowLayout.LEFT);
 		EnoncePanel.add(ExerciceNbTitlePanel);
-		
+
 		JLabel lblExercice = new JLabel("Exercice 1");
 		ExerciceNbTitlePanel.add(lblExercice);
 		lblExercice.setAutoscrolls(true);
 		lblExercice.setAlignmentY(Component.TOP_ALIGNMENT);
-		
+
 		JTextPane txtpnTitreDeLexercice = new JTextPane();
 		txtpnTitreDeLexercice.setText("Titre de l'exercice");
 		ExerciceNbTitlePanel.add(txtpnTitreDeLexercice);
-		
+
 		JPanel QuestionNbTitlePanel = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) QuestionNbTitlePanel.getLayout();
 		flowLayout.setAlignment(FlowLayout.LEFT);
 		EnoncePanel.add(QuestionNbTitlePanel);
-		
+
 		JLabel lblQuestion = new JLabel("Question 1");
 		QuestionNbTitlePanel.add(lblQuestion);
-		
+
 		JTextPane txtpnTitreDeLa = new JTextPane();
 		txtpnTitreDeLa.setText("Titre de la question(si n\u00E9cessaire, ne pas toucher sinon)");
 		QuestionNbTitlePanel.add(txtpnTitreDeLa);
-		
+
 		JTextPane Enoncé = new JTextPane();
 		Enoncé.setText("Entrer l'\u00E9nonc\u00E9 de la question");
 		EnoncePanel.add(Enoncé);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		frmTest.setJMenuBar(menuBar);
-		
+
 		JMenu mnFichier = new JMenu("Fichier");
 		menuBar.add(mnFichier);
-		
+
 		JMenuItem mntmNouveau = new JMenuItem("Nouveau");
 		mntmNouveau.addActionListener(new ActionListener() {
 			@Override
@@ -329,7 +452,7 @@ public class TestWindow {
 			}
 		});
 		mnFichier.add(mntmNouveau);
-		
+
 		JMenuItem mntmOuvrir = new JMenuItem("Ouvrir");
 		mntmOuvrir.addActionListener(new ActionListener() {
 			@Override
@@ -338,7 +461,7 @@ public class TestWindow {
 			}
 		});
 		mnFichier.add(mntmOuvrir);
-		
+
 		JMenuItem mntmEnregistrer = new JMenuItem("Enregistrer");
 		mntmEnregistrer.addActionListener(new ActionListener() {
 			@Override
@@ -347,14 +470,14 @@ public class TestWindow {
 			}
 		});
 		mnFichier.add(mntmEnregistrer);
-		
+
 		JCheckBoxMenuItem chckbxmntmEnregistrementAuto = new JCheckBoxMenuItem("Enregistrement auto");
 		mnFichier.add(chckbxmntmEnregistrementAuto);
 		chckbxmntmEnregistrementAuto.setSelected(true);
-		
+
 		JMenu mnModifier = new JMenu("Modifier");
 		menuBar.add(mnModifier);
-		
+
 		JMenuItem mntmModifierLeFormulaire = new JMenuItem("Modifier le formulaire de contact");
 		mnModifier.add(mntmModifierLeFormulaire);
 		mntmModifierLeFormulaire.addActionListener(new ActionListener() {
@@ -363,22 +486,23 @@ public class TestWindow {
 				JOptionPane.showMessageDialog(null, "Ca s'ouvre");
 			}
 		});
-		
+
 		JMenu mnMode = new JMenu("Mode");
 		menuBar.add(mnMode);
-		
+
 		JRadioButtonMenuItem rdbtnmntmExamen = new JRadioButtonMenuItem("Examen");
 		ExerciseMode.add(rdbtnmntmExamen);
 		mnMode.add(rdbtnmntmExamen);
-		rdbtnmntmExamen.setSelected(true); //valeur par defaut du groupe de boutons
-		
+		rdbtnmntmExamen.setSelected(true); // valeur par defaut du groupe de
+											// boutons
+
 		JRadioButtonMenuItem rdbtnmntmExercice = new JRadioButtonMenuItem("Exercice");
 		ExerciseMode.add(rdbtnmntmExercice);
 		mnMode.add(rdbtnmntmExercice);
-		
+
 		JMenu mnAide = new JMenu("Aide");
 		menuBar.add(mnAide);
-		
+
 		JMenuItem mntmAPropos = new JMenuItem("A propos");
 		mntmAPropos.addActionListener(new ActionListener() {
 			@Override
@@ -387,7 +511,7 @@ public class TestWindow {
 			}
 		});
 		mnAide.add(mntmAPropos);
-		
+
 		JMenuItem mntmGnrerLexercice = new JMenuItem("G\u00E9n\u00E9rer l'exercice");
 		menuBar.add(mntmGnrerLexercice);
 		mntmGnrerLexercice.addActionListener(new ActionListener() {
@@ -396,33 +520,33 @@ public class TestWindow {
 				JOptionPane.showMessageDialog(null, "Ca s'ouvre");
 			}
 		});
-		
+
 		JPanel TimerPanel = new JPanel();
 		FlowLayout fl_TimerPanel = (FlowLayout) TimerPanel.getLayout();
 		fl_TimerPanel.setAlignment(FlowLayout.RIGHT);
 		menuBar.add(TimerPanel);
-		
+
 		JLabel lblMinuteur = new JLabel("Minuteur:");
 		TimerPanel.add(lblMinuteur);
-		
+
 		JSpinner spinner = new JSpinner();
 		spinner.setModel(new SpinnerNumberModel(0, 0, 23, 1));
 		TimerPanel.add(spinner);
-		
+
 		JLabel lblH = new JLabel("h");
 		TimerPanel.add(lblH);
-		
+
 		JSpinner spinner_1 = new JSpinner();
 		spinner_1.setModel(new SpinnerNumberModel(0, 0, 59, 1));
 		TimerPanel.add(spinner_1);
-		
+
 		JLabel lblMin = new JLabel("min");
 		TimerPanel.add(lblMin);
-		
+
 		JSpinner spinner_2 = new JSpinner();
 		spinner_2.setModel(new SpinnerNumberModel(30, 0, 59, 1));
 		TimerPanel.add(spinner_2);
-		
+
 		JLabel lblS = new JLabel("s");
 		TimerPanel.add(lblS);
 	}
@@ -432,6 +556,7 @@ public class TestWindow {
 			putValue(NAME, "SwingAction");
 			putValue(SHORT_DESCRIPTION, "Some short description");
 		}
+
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
