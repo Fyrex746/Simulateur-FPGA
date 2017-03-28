315918a46c447ed0bf48e6c9ef8ea17b2cc45675
Fyrex746 <louisquelliermsn@hotmail.fr>
2017-03-28 17:01:44 +0200

    add interface Simulator
    Ajoute l'interface Simulateur pour communiquer avec l'interface
    graphique

---
+++ FPGA Simulator/src/component/Simulator.java
@@ -0,0 +1,18 @@
+package component;
+import java.util.ArrayList;
+import java.util.Map;
+
+public interface Simulator {
+	//Appeler cette fonction dans l'interface graphique pour avoir le nom des différents paramètres demandés par le simulateur
+	public ArrayList<String> getStrParameters();
+	
+	//Appeler cette fonction dans l'interface graphique pour configurer les paramètres du simulateur et gérer les erreurs
+	public void setParameters(Map<String, String> parameters) throws SimulatorParametersException;
+}
+
+class SimulatorParametersException  extends Exception{
+	//A sous-classer dans les classes implémentant Simulator afin de lever des exeptions personaliser
+	public SimulatorParametersException(){
+		System.out.println("Paramètre du simulateur incorrect");
+	}
+}
