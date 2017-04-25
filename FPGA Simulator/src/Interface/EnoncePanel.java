16fe34074ab4de7f83bc7c78d50bca3771899b52
Dany Sysavath <Dany Sysavath@SsdPortableDany>
2017-04-25 14:00:13 +0200

    Titres sans retour à la ligne + JScrollPane pour l'énoncé

--- FPGA Simulator/src/Interface/EnoncePanel.java
+++ FPGA Simulator/src/Interface/EnoncePanel.java
@@ -1,9 +1,16 @@
 package Interface;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import java.awt.BorderLayout;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
 import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JLabel;
 import java.awt.FlowLayout;
 import javax.swing.JFormattedTextField;
@@ -31,7 +38,8 @@ public class EnoncePanel extends JPanel {
 		JLabel lblExercice = new JLabel("Exercice "+nbEx);
 		ExNbPanel.add(lblExercice);
 		
-		JTextArea txtrTitreDeLexercice = new JTextArea();
+		//JTextArea txtrTitreDeLexercice = new JTextArea(); //retour a la ligne possible
+		JTextField txtrTitreDeLexercice = new JTextField(); //retour a la ligne impossible
 		ExNbPanel.add(txtrTitreDeLexercice);
 		txtrTitreDeLexercice.setText("Titre de l'exercice");
 		
@@ -43,15 +51,21 @@ public class EnoncePanel extends JPanel {
 		JLabel lblQuestion = new JLabel("Question "+nbQu);
 		QuNbPanel.add(lblQuestion);
 		
-		JTextArea txtrTitreDeLa = new JTextArea();
+		//JTextArea txtrTitreDeLa = new JTextArea(); //retour a la ligne possible
+		JTextField txtrTitreDeLa = new JTextField(); //retour a la ligne impossible
 		txtrTitreDeLa.setText("Titre de la question (si n\u00E9cessaire)");
 		QuNbPanel.add(txtrTitreDeLa);
 		
 		JTextArea enonce = new JTextArea();
 		enonce.setBackground(Color.LIGHT_GRAY);
 		enonce.setText("Entrer l'\u00E9nonc\u00E9 de la question ici");
-		add(enonce, BorderLayout.CENTER);
+		//add(enonce, BorderLayout.CENTER);
 
+		JScrollPane jsp = new JScrollPane (enonce);
+		jsp.setPreferredSize(new Dimension(10,50));
+		jsp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	    jsp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+	    add(jsp, BorderLayout.CENTER);
 	}
 
 }
